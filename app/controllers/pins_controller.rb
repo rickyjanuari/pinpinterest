3a9b301f73da4d33be00aaa0a108d2126f4c80ce
rickyjanuari <ricky0101@gmail.com>
2015-04-16 12:52:31 +0700

    Add devise pin and user association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -9,11 +9,11 @@ class PinsController < ApplicationController
 	end
 	
 	def new
-		@pin = Pin.new
+		@pin = current_user.pins.build
 	end
 
 	def create
-		@pin = Pin.new(pin_params)
+		@pin = current_user.pins.build(pin_params)
 		
 		if @pin.save
 			redirect_to @pin, notice: "Successfully Created New Pin"
@@ -36,6 +36,7 @@ class PinsController < ApplicationController
 	def destroy
 		@pin.destroy
 		redirect_to root_path
+
 	end
 
 	private
